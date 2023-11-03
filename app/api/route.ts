6a78d3f99ec9a1dd9f34dd7274b6475eedd20063
박수연 <sksrbdpdy@naver.com>
2023-11-03 19:27:48 +0900

    운세

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,12 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) : 
 Promise<NextResponse> =>{
-    const query = req.nextUrl.searchParams
+
+    const query = req.nextUrl.searchParams;
     const gender = query.get("gender")
     const birthdate = query.get("birthdate")
     const month = query.get("month");
     const time = query.get("time");
 
     console.log(gender, birthdate, month, time)
+
+    const res = await fetch(`https://m.search.naver.com/p/csearch/dcontent/external_api/json_todayunse_v2.naver?_callback=window.__jindo2_callback._fortune_my_0&gender=${gender}&birth=${birthdate}&solarCal=${month}&time=${time}`)
+    // https://m.search.naver.com/p/csearch/dcontent/external_api/json_todayunse_v2.naver
+    console.log(res)
+
     return NextResponse.json({error: "데이터가 없음"})
 }
